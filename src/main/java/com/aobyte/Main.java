2e39eeb6fb50f15647b9481a6bf9f30b35a7ead5
Silvia Abrahamyan <silvia.abrahamyan1996@gmail.com>
2019-02-05 03:43:31 -0800

    init

--- src/main/java/com/aobyte/Main.java
+++ src/main/java/com/aobyte/Main.java
@@ -4,6 +4,7 @@ package com.aobyte;
  * Created by home on 2/3/2019.
  */
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello from Frontend");
     }
